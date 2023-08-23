ffe3f5f96ccc8153cd88c7582127f27a1acbd436
Seonz <zxzx00202@naver.com>
2023-08-23 21:16:36 +0900

    백 뼈대 만듦

---
+++ back/neterpark/src/main/java/com/spr/neterpark/NeterparkApplication.java
@@ -0,0 +1,13 @@
+package com.spr.neterpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeterparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeterparkApplication.class, args);
+	}
+
+}
---
+++ back/neterpark/src/test/java/com/spr/neterpark/NeterparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spr.neterpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeterparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
