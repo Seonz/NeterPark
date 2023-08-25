c8a47177c35b650d91d4521a2af8746bc5240668
ovo <audwldhks0@gmaio.com>
2023-08-25 12:48:48 +0900

    axios test

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,9 +5,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Components from "./pages/home";
 import "./assets/scss/style.scss";
+// 전역 axios 설정
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8095";
+axios.defaults.withCredentials = true; // withCredentials 전역 설정
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+axios.get('/main')
+  .then((res)=>{console.log(res)})
+  .catch((err)=>{console.log(err)})
 
 root.render(
   <BrowserRouter>
