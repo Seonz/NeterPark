8445d547f69c02aea6e30c424fb28596e86dd0da
trend7936 <trend7936@naver.com>
2023-08-31 20:33:55 +0900

    entity부분변경

--- back/neterpark/src/main/java/com/spr/neterpark/entity/Notice.java
+++ back/neterpark/src/main/java/com/spr/neterpark/entity/Notice.java
@@ -2,7 +2,7 @@ package com.spr.neterpark.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-
+import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import java.util.Date;
 
@@ -28,7 +28,7 @@ public class Notice {
     @JoinColumn(name = "userId", insertable = false, updatable = false)
     private User user; // 아이디
 
-    @Column(length = 18, nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
     private Date regDate; // 작성 일자
 }
